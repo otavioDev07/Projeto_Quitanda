33f97ef938d1110e8fec3e995c188ee31ef32d21
Otávio Neto <netootavio223@outlook.com>
2023-11-07 13:56:46 -0300

    eliminação da página contato

--- Quitanda_do_Zé/app.py
+++ Quitanda_do_Zé/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, Blueprint, session, render_template, redirect
 from quem_somos.quem_somos import quemsomos_blueprint
 from home.home import home_blueprint
-from contato.contato import contato_blueprint
 from admin.admin import admin_blueprint
 from model.modelo import modelo_blueprint
 from database.conexao import database_blueprint
@@ -10,7 +9,6 @@ app = Flask(__name__)
 app.secret_key = 'seuzequitanda'
 app.register_blueprint(quemsomos_blueprint) #Conecta a blueprint importada no arquivo principal
 app.register_blueprint(home_blueprint)
-app.register_blueprint(contato_blueprint)
 app.register_blueprint(admin_blueprint)
 app.register_blueprint(modelo_blueprint)
 app.register_blueprint(database_blueprint)
--- Quitanda_do_Zé/contato/contato.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, Blueprint
-
-contato_blueprint = Blueprint('contato', __name__, template_folder='templates')
-
-@contato_blueprint.route('/contato')
-def contato():
-    titulo = 'CONTATO'
-    return render_template('contato.html', titulo=titulo)
