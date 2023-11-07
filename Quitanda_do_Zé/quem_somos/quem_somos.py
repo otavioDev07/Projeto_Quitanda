d26ecf6bf40d214a8c51b53a483e31b227bdc7da
Otávio Neto <netootavio223@outlook.com>
2023-11-07 10:08:26 -0300

    .

--- Quitanda_do_Zé/quem_somos/quem_somos.py
+++ Quitanda_do_Zé/quem_somos/quem_somos.py
@@ -2,4 +2,9 @@ from flask import Flask, render_template, Blueprint
 
 quemsomos_blueprint = Blueprint('quem_somos', __name__, template_folder='templates')
 
+@quemsomos_blueprint.route('/sobre')
+def sobre():
+    titulo = 'QUEM SOMOS'
+    return render_template('sobre.html', titulo=titulo)
+
 
