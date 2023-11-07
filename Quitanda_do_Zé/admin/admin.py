615a823cd02b67a6402b852adcffe091cbb8b751
Otávio Neto <netootavio223@outlook.com>
2023-11-07 15:57:32 -0300

    .

--- Quitanda_do_Zé/admin/admin.py
+++ Quitanda_do_Zé/admin/admin.py
@@ -1,3 +1,8 @@
 from flask import Flask, render_template, Blueprint, request, redirect, session
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
+
+@admin_blueprint.route('/login')
+def login():
+    titulo = 'LOGIN'
+    return render_template('login.html', titulo=titulo)
--- Quitanda_do_Zé/home/home.py
+++ Quitanda_do_Zé/home/home.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, Blueprint
+from database.conexao import iniciar_db, get_db_conexao
+from session.session import verifica_sessao
 
 home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 
 @home_blueprint.route("/")
 def index():
+    global logado
+    iniciar_db()
+    conexao = get_db_conexao()
+    produtos = conexao.execute('SELECT * FROM produtos ORDER BY id DESC').fetchall()
+    conexao.close() 
+    #Verifica a sessão
+    if verifica_sessao():
+        logado = True
+    else:
+        logado = False
     titulo = 'PÁGINA INICIAL'
     return render_template('index.html', titulo=titulo)
