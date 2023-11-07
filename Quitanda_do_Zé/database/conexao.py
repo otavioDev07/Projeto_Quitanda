3730c84d91882dc27089cf12701d854a08273d8a
Otávio Neto <netootavio223@outlook.com>
2023-11-07 13:04:28 -0300

    banco de dados

--- Quitanda_do_Zé/database/conexao.py
+++ Quitanda_do_Zé/database/conexao.py
@@ -6,14 +6,14 @@ database_blueprint = Blueprint('database', __name__,)
 
 #Conexão com o banco de dados
 def get_db_conexao():
-    db_path = os.path.join(app.root_path, 'dados.db')
+    db_path = os.path.join(database_blueprint.root_path, 'dados.db')
     conexao = sql.connect(db_path)
     conexao.row_factory = sql.Row
     return conexao
 
 def iniciar_db():
     conexao = get_db_conexao()
-    with app.open_resource('esquema.sql', mode='r') as comandos:
+    with database_blueprint.open_resource('esquema.sql', mode='r') as comandos:
         conexao.cursor().executescript(comandos.read())
     conexao.commit()
     conexao.close()
