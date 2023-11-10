513f2b6f2815601943921d386043a929e4d3a406
Otávio Neto <netootavio223@outlook.com>
2023-11-10 13:32:21 -0300

    .

--- Quitanda_do_Zé/admin/admin.py
+++ Quitanda_do_Zé/admin/admin.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Blueprint, request, redirect, session
 from session.session import verifica_sessao
 from database.conexao import iniciar_db, get_db_conexao 
+import uuid
 
 usuario = 'adm'
 senha = '1234'
@@ -37,5 +38,36 @@ def adm():
         return render_template('adm.html', produtos=produtos, title=title)
     else:
         return redirect('/login')
+    
+
+#Rota para renderizar página de cadastro
+@admin_blueprint.route('/cadprodutos')
+def cadprodutos():
+    if verifica_sessao():
+        title = "CADASTRO DE PRODUTOS"
+        return render_template('cadastro.html', title=title)
+    else:
+        return redirect('/login')
+    
+#Rota para cadastro no database
+@admin_blueprint.route('/cadastro', methods=['post'])
+def cadastro():
+    if verifica_sessao():
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        preco = request.form['preco']
+        img = request.files['img']
+        id_img = str(uuid.uuid4().hex)
+        filename = id_img+nome+'.png'
+        img.save('../../static/img/produtos')
+        conexao = get_db_conexao()
+        conexao.execute('INSERT INTO produtos (nome, descricao, preco, img) VALUES (?, ?, ?, ?)', (nome, descricao, preco, filename))
+        conexao.commit()
+        conexao.close()
+        return redirect('/adm')
+    else:
+        return redirect('/login')
+    
+
 
     
